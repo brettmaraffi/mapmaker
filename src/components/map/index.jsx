cbb35b97e1e635e65984f02d43f76bca2d43c2a9
brettmaraffi <36203961+brettmaraffi@users.noreply.github.com>
2020-09-29 16:03:04 -0400

    Add files via upload

---
+++ src/components/map/index.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+export default function Map({tiles, setTiles, activeTile, tileset, size, bgTile}) {
+    function cloneMatrix(m) {
+        const clone = new Array(m.length);
+        for(let i = 0; i < m.length; i++) {
+            clone[i] = m[i].slice(0);
+        }
+        return clone;
+    }
+
+    function dropTile({x, y}) {
+        // clone matrix, alter tile @ x/y to the active tile
+        setTiles((prev) => {
+            const clone = cloneMatrix(prev);
+            const tile = {
+                ...clone[y][x],
+                v: activeTile
+            };
+            clone[y][x] = tile;
+            return clone;
+        });
+    }
+
+    return (
+        <div
+            style={{
+                boxSizing: "border-box",
+                backgroundColor: "white",
+                width: size.width,
+            }}
+        >
+            <div style={{ position: "absolute", zIndex: 1 }}>
+                {
+                tiles.map((row, y) => (
+                    <div style={{ display: "flex" }}>
+                    {
+                        row.map((tile, x) => (
+                        <div 
+                            onClick={() => dropTile({ x, y })}
+                            style={{
+                                borderTop: "1px solid black",
+                                borderRight: "1px solid black",
+                                background: `url(/sprites/${tileset}.png) 
+                                            -${bgTile.x}px 
+                                            -${bgTile.y}px 
+                                            no-repeat`,
+                                width: 32,
+                                height: 32,
+                            }} 
+                        />
+                        ))
+                    }
+                    </div>
+                    ))
+                }
+            </div>
+
+            <div style={{ position: "absolute", zIndex: 2 }}>
+                {
+                tiles.map((row, y) => (
+                    <div style={{ display: "flex" }}>
+                    {
+                        row.map((tile, x) => (
+                        <div 
+                            onClick={() => dropTile({ x, y })}
+                            style={{
+                                borderTop: "1px solid black",
+                                borderRight: "1px solid black",
+                                background: `url(/sprites/${tileset}.png) 
+                                            -${tile.v.x}px 
+                                            -${tile.v.y}px 
+                                            no-repeat`,
+                                width: 32,
+                                height: 32,
+                            }} 
+                        />
+                        ))
+                    }
+                    </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
